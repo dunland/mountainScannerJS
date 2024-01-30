01681a6b278839edba66546a936e0c6ff11f2ead
dunland <david.unland@web.de>
2024-01-30 11:09:55 +0100

    webmidi works with VOLCA keys!

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # mountainScannerJS
 
+- uses nodeJS for the `requestAnimationFrame` function
+- [midijs library](https://webmidijs.org/docs/) works on Google Chrome only!
+
 ## run
 
 - `npm start` to just start
 - `yarn server` to start & keep track of code changes (dev mode)
+
+## problems
+
+> SecurityError: WebMIDI requires a site permission add-on to activate
--- public/scanner.js
+++ public/scanner.js
@@ -1,58 +1,94 @@
-const movingRegion = document.querySelector('.moving-region');
 var values, centerX;
 // import { WebMidi } from "./webmidi/dist/iife/webmidi.iife";
-import {WebMidi} from "./webmidi/dist/esm/webmidi.esm.js";
-
+import { WebMidi } from "./webmidi/dist/esm/webmidi.esm.js";
+var mySynth;
 // ---------------- Script wrapper: ------------------
 document.addEventListener("DOMContentLoaded", () => {
 
-fetch("./data.json")
+  fetch("./data.json")
     .then((res) => res.json())
     .then((data) => {
-        values = data.values;
+      values = data.values;
 
-        WebMidi
+      // Enable WEBMIDI.js and trigger the onEnabled() function when ready
+      WebMidi
         .enable()
-        .then(onMidiEnabled)
-        .catch(err => alert(err));
+        .then(onEnabled)
+        // .catch(err => alert(err));
+        .then(() => {
+          moveRegion();
+        })
 
-        moveRegion();
+
+      // Function triggered when WEBMIDI.js is ready
+      function onEnabled() {
+
+        // Display available MIDI input devices
+        if (WebMidi.inputs.length < 1) {
+          document.body.innerHTML += "No device detected.";
+        } else {
+          WebMidi.inputs.forEach((device, index) => {
+            console.log("Midi Inputs:" + `${index}: ${device.name}`);
+          });
+
+          // mySynth = WebMidi.inputs[0];
+          // const mySynth = WebMidi.getInputByName("TYPE NAME HERE!")
+        }
+
+        if (WebMidi.outputs.length < 1) {
+          console.log("no output devices detected.");
+        }
+        else {
+          console.log("Midi Outputs:");
+          WebMidi.outputs.forEach((device, index) => {
+           console.log(`${index}: ${device.name}`);
+          });
+
+          // mySynth = WebMidi.outputs[1];
+          mySynth = WebMidi.getOutputByName("USB MIDI Interface MIDI 1");
+        }
+      }
+
+      console.log(WebMidi.inputs);
+
     })
 
-function onMidiEnabled() {
-    console.log(WebMidi.inputs);
-    console.log(WebMidi.outputs);
-    }
+  // ------------------- functions: --------------------
+  function moveRegion() {
 
-// ------------------- functions: --------------------
-function moveRegion() {
     let position = 0, regionWidth = 20;
     const screenWidth = window.innerWidth;
     console.log(innerWidth, screenWidth);
-    const moveSpeed = 1; // Adjust the speed of the moving region
+    const moveSpeed = 50; // Adjust the speed of the moving region
 
     function animate() {
-        position += moveSpeed;
-        centerX = position + regionWidth / 2;
-        if (position > screenWidth) {
-            position = -regionWidth; // Reset the position when the region moves out of the screen
-        }
+      position += moveSpeed;
+      centerX = position + regionWidth / 2;
+      if (position > screenWidth) {
+        position = -regionWidth; // Reset the position when the region moves out of the screen
+      }
 
-        const index = Math.floor((centerX / screenWidth) * values.length);
-        const value = values[index];
-        displayValue(centerX, value);
+      const index = Math.floor((centerX / screenWidth) * values.length);
+      const value = values[index % values.length];
+      const valueAsMidi = 127 - (value/1080 * 127);
+      displayValue(centerX, value);
 
-        movingRegion.style.left = position + 'px';
-        let xpos = screenWidth - position;
-        movingRegion.style.backgroundPosition = `${xpos}px 0px`;
-        requestAnimationFrame(animate);
+      // console.log(`value ${value} as midi: ${ 127 - (value/1080 * 127)}`);
+      mySynth.sendControlChange(44, valueAsMidi);
+      // WebMidi.outputs[1].sendNoteOn(127 - (value/1080 * 127));
+
+      const movingRegion = document.querySelector('.moving-region');
+      movingRegion.style.left = position + 'px';
+      let xpos = screenWidth - position;
+      movingRegion.style.backgroundPosition = `${xpos}px 0px`;
+      requestAnimationFrame(animate);
     }
 
     animate();
-}
+  }
 
-// TODO: Function to invert the pixels of the underlying image
-function invertImage() {
+  // TODO: Function to invert the pixels of the underlying image
+  function invertImage() {
     const fullscreenImage = document.querySelector('.fullscreen-image');
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
@@ -64,19 +100,19 @@ function invertImage() {
     const data = imageData.data;
 
     for (let i = 0; i < data.length; i += 4) {
-        data[i] = 255 - data[i]; // Invert the red channel
-        data[i + 1] = 255 - data[i + 1]; // Invert the green channel
-        data[i + 2] = 255 - data[i + 2]; // Invert the blue channel
-        // Alpha channel (data[i + 3]) is unchanged
+      data[i] = 255 - data[i]; // Invert the red channel
+      data[i + 1] = 255 - data[i + 1]; // Invert the green channel
+      data[i + 2] = 255 - data[i + 2]; // Invert the blue channel
+      // Alpha channel (data[i + 3]) is unchanged
     }
 
     ctx.putImageData(imageData, 0, 0);
     // fullscreenImage.src = canvas.toDataURL(); // Update the image with inverted pixels
     // TODO:
     // movingRegion.style.backgroundImage = imageData;
-}
+  }
 
-function displayValue(x, y) {
+  function displayValue(x, y) {
 
     const valueElement = document.querySelector('.value-display');
 
@@ -84,5 +120,5 @@ function displayValue(x, y) {
     valueElement.style.left = x + 15 + 'px';
     valueElement.style.top = y - 15 + 'px';
     valueElement.textContent = '___' + y;
-}
+  }
 })
