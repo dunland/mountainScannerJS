cddbbd80342399f250f57230d7d78347f930190e
David Unland <david.unland@web.de>
2024-07-05 10:49:13 +0200

    webMidi class

--- public/webMidi.js
+++ public/webMidi.js
@@ -1,42 +1,47 @@
 import { WebMidi } from "./webmidi/dist/esm/webmidi.esm.js";
 
-export function loadWebMidi(){
-  try {
+export class Midi {
 
-    // Enable WEBMIDI.js and trigger the onEnabled() function when ready
-    WebMidi
-      .enable()
-      .then(() => {
-        // Display available MIDI input devices
-        if (WebMidi.inputs.length < 1) {
-          document.body.innerHTML += "No device detected.";
-        } else {
-          WebMidi.inputs.forEach((device, index) => {
-            console.log("Midi Inputs:" + `${index}: ${device.name}`);
-          });
+  synth;
 
-          // mySynth = WebMidi.inputs[0];
-          // const mySynth = WebMidi.getInputByName("TYPE NAME HERE!")
-        }
+  static loadWebMidi() {
+    try {
 
-        if (WebMidi.outputs.length < 1) {
-          console.log("no output devices detected.");
-        }
-        else {
-          console.log("Midi Outputs:");
-          WebMidi.outputs.forEach((device, index) => {
-            console.log(`${index}: ${device.name}`);
-          });
+      // Enable WEBMIDI.js and trigger the onEnabled() function when ready
+      WebMidi
+        .enable()
+        .then(() => {
+          // Display available MIDI input devices
+          if (WebMidi.inputs.length < 1) {
+            document.body.innerHTML += "No device detected.";
+          } else {
+            WebMidi.inputs.forEach((device, index) => {
+              console.log("Midi Inputs:" + `${index}: ${device.name}`);
+            });
 
-          // mySynth = WebMidi.outputs[1];
-          mySynth = WebMidi.getOutputByName("USB MIDI Interface MIDI 1");
-        }
+            // this.synth = WebMidi.inputs[0];
+            // this.synth = WebMidi.getInputByName("TYPE NAME HERE!")
+          }
 
-      }) // init output synth
-      .catch(err => alert(err));
+          if (WebMidi.outputs.length < 1) {
+            console.log("no output devices detected.");
+          }
+          else {
+            console.log("Midi Outputs:");
+            WebMidi.outputs.forEach((device, index) => {
+              console.log(`${index}: ${device.name}`);
+            });
 
-    console.log(`WebMidi.inputs: ${WebMidi.inputs}`);
-  } catch (error) {
-    console.error("could not initialize MIDI.", error);
+            // this.synth = WebMidi.outputs[1];
+            this.synth = WebMidi.getOutputByName("USB MIDI Interface MIDI 1");
+          }
+
+        }) // init output synth
+        .catch(err => alert(err));
+
+      console.log(`WebMidi.inputs: ${WebMidi.inputs}`);
+    } catch (error) {
+      console.error("could not initialize MIDI.", error);
+    }
   }
 }
